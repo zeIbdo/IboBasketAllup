5541ef6263aaad29ab5669820a8df7cf513e25d5
zeIbdo <ibrahimrm@code.edu.az>
2024-12-02 14:03:17 +0400

    done (code got prettier)

--- Allup.Application/Services/Abstracts/IBasketService.cs
+++ Allup.Application/Services/Abstracts/IBasketService.cs
@@ -4,6 +4,7 @@ namespace Allup.Application.Services.Abstracts;
 
 public interface IBasketService
 {
-	Task<int> AddToBasketAsync(int productId);
-	Task<int> RemoveFromBasketAsync(int productId);
+    Task<int> AddToBasketAsync(int productId);
+    Task<int> RemoveFromBasketAsync(int productId);
+    Task<BasketViewModel> GetBasketViewModelAsync();
 }
--- Allup.Application/Services/Implementations/BasketManager.cs
+++ Allup.Application/Services/Implementations/BasketManager.cs
@@ -1,51 +1,92 @@
 ﻿using Allup.Application.Services.Abstracts;
 using Allup.Application.UI.Services.Abstracts;
+using Allup.Application.UI.Services.Implementations;
 using Allup.Application.UI.ViewModels;
 using Allup.Application.ViewModels;
 using Allup.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 using System.Security.Claims;
 
 namespace Allup.Application.Services.Implementations;
 
 public class BasketManager : IBasketService
 {
-	private readonly IHttpContextAccessor _contextAccessor;
-	private readonly ICookieService _cookieService;
-	private readonly IBasketItemService _basketItemService;
+    private readonly IHttpContextAccessor _contextAccessor;
+    private readonly ICookieService _cookieService;
+    private readonly IBasketItemService _basketItemService;
+    private readonly ExternalApiService _externalApiService;
 
 
-	public BasketManager(IHttpContextAccessor contextAccessor, ICookieService cookieService, IBasketItemService basketItemService, UserManager<AppUser> userService)
-	{
-		_contextAccessor = contextAccessor;
-		_cookieService = cookieService;
-		_basketItemService = basketItemService;
-	}
+    public BasketManager(IHttpContextAccessor contextAccessor, ICookieService cookieService, IBasketItemService basketItemService, UserManager<AppUser> userService, ExternalApiService externalApiService)
+    {
+        _contextAccessor = contextAccessor;
+        _cookieService = cookieService;
+        _basketItemService = basketItemService;
+        _externalApiService = externalApiService;
+    }
 
-	public async Task<int> AddToBasketAsync(int productId)
-	{
-		string clientId = "";
-		if (!_contextAccessor.HttpContext.User.Identity!.IsAuthenticated)
-			 clientId = _cookieService.GetBrowserId();			
-		else
-			 clientId =  _contextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+    public async Task<int> AddToBasketAsync(int productId)
+    {
+        string clientId = "";
+        if (!_contextAccessor.HttpContext.User.Identity!.IsAuthenticated)
+            clientId = _cookieService.GetBrowserId();
+        else
+            clientId = _contextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)!;
 
-		var basketCreateViewModel = new BasketItemCreateViewModel { ProductId = productId, ClientId = clientId, Count = 1 };
-		await _basketItemService.CreateAsync(basketCreateViewModel);
-		var count = (await _basketItemService.GetAllAsync(x => x.ClientId == clientId)).Count;
-		return count;
-	}
+        var basketCreateViewModel = new BasketItemCreateViewModel { ProductId = productId, ClientId = clientId, Count = 1 };
+        await _basketItemService.CreateAsync(basketCreateViewModel);
+        var count = (await _basketItemService.GetAllAsync(x => x.ClientId == clientId)).Count;
+        return count;
+    }
 
-	public async Task<int> RemoveFromBasketAsync(int id)
-	{
-		string clientId = "";
-		if (!_contextAccessor.HttpContext.User.Identity!.IsAuthenticated)
-			clientId = _cookieService.GetBrowserId();
-		else
-			clientId = _contextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)!;
-		await _basketItemService.DeleteAsync(id);
-		var count = (await _basketItemService.GetAllAsync(x => x.ClientId == clientId)).Count;
-		return count;
-	}
+    public async Task<BasketViewModel> GetBasketViewModelAsync()
+    {
+        var language = await _cookieService.GetLanguageAsync();
+        string clientId = "";
+
+        if (!_contextAccessor.HttpContext.User.Identity!.IsAuthenticated)
+             clientId = _cookieService.GetBrowserId();
+        else
+             clientId =_contextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+        var items = await _basketItemService.GetAllAsync(x => x.ClientId == clientId, include: x => x.Include(y => y.Product!).ThenInclude(x => x.ProductTranslations!.Where(x => x.LanguageId == language.Id)));
+        var basketProducts = new List<BasketProductViewModel>();
+        foreach (var item in items)
+        {
+            var basketProduct = new BasketProductViewModel
+            {
+                BasketItemId = item.Id,
+                Count = item.Count,
+                ProductId = item.ProductId,
+                Name = item.Product!.Name,
+                Price = item.Product.Price,
+                CoverImageUrl = item.Product.CoverImageUrl,
+                FormattedPrice = item.Product.FormattedPrice,
+            };
+            basketProducts.Add(basketProduct);
+        }
+        var basketViewModel = new BasketViewModel() { Items = basketProducts };
+        var currency = await _cookieService.GetCurrencyAsync();
+
+        var coefficient = await _externalApiService.GetCurrencyCoefficient(currency.CurrencyCode ?? "azn");
+        var culture = new CultureInfo(currency.IsoCode ?? "az-az");
+
+        var totalAmount = (basketViewModel.TotalAmount / coefficient).ToString("C", culture);
+        basketViewModel.FormattedTotalAmount = totalAmount;
+        return basketViewModel;
+    }
+
+    public async Task<int> RemoveFromBasketAsync(int id)
+    {
+        string clientId = "";
+        if (!_contextAccessor.HttpContext.User.Identity!.IsAuthenticated)
+            clientId = _cookieService.GetBrowserId();
+        else
+            clientId = _contextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+        await _basketItemService.DeleteAsync(id);
+        var count = (await _basketItemService.GetAllAsync(x => x.ClientId == clientId)).Count;
+        return count;
+    }
 }
